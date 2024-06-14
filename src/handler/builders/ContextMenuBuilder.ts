fde7763b1985eb0a35786cab89cad4f375d74867
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-06-14 22:12:28 +0200

    Changed it to match commands, since thats basiclaly how they be like

--- src/handler/builders/ContextMenuBuilder.ts
+++ src/handler/builders/ContextMenuBuilder.ts
@@ -13,25 +13,26 @@ export class ContextMenuBuilder {
     isCommand: boolean = false; // This is so that when it's read by th command handler, it's skipped. And because it's its own interaction like slash commands, the interaction handler reads it seprately.
     type: ApplicationCommandType;
     name: string;
-    callback: (interaction: ContextMenuCommandInteraction) => void;
+    callback: (
+        interaction: ContextMenuCommandInteraction,
+        client?: Client
+    ) => void;
     deleted: boolean;
 
     /**
-     *
-     * @param name Name of the context menu.
+     * Build the actual context menu.
+     * @param context Context Menu Object
      */
-    constructor(name: string) {
-        this.name = name;
-    }
-
-    /**
-     * Sets the type of the context menu
-     * @param type Context Menu type.
-     * @returns
-     */
-    setType(type: ApplicationCommandType): ContextMenuBuilder {
-        this.type = type;
-        return this;
+    constructor(context: {
+        data: ContextMenuCommandBuilder;
+        execute: (
+            interaction: ContextMenuCommandInteraction,
+            client?: Client
+        ) => void;
+    }) {
+        this.name = context.data.toJSON().name;
+        this.type = context.data.toJSON().type;
+        this.callback = context.execute;
     }
 
     /**
