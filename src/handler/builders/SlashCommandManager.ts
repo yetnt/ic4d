4a3863db55737bebeea60c32645d90016369488f
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-06-26 10:42:01 +0200

    deprecated warning

--- src/handler/builders/SlashCommandManager.ts
+++ src/handler/builders/SlashCommandManager.ts
@@ -61,6 +61,10 @@ export class SlashCommandManager {
      * Whether the command is deleted or not
      */
     deleted: boolean;
+    /**
+     *
+     */
+    protected isOld: boolean = false;
 
     constructor(commandObject: {
         /**
